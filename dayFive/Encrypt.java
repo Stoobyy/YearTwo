327e58f386393ea1ffa4f632329fcf267cfb6bfc
Stooby with two y's <97161453+Stoobyy@users.noreply.github.com>
2024-09-29 17:34:52 +0530

    fuck file handling

---
+++ dayFive/Encrypt.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Encrypt {
+    public static void main(String args[]) throws IOException {
+        String inputFile = "message.txt";
+        String outputFile = "encrypted.txt";
+        try {
+            FileReader reader = new FileReader(inputFile);
+            FileWriter writer = new FileWriter(outputFile);
+            int c;
+            while((c=reader.read()) != -1)
+            {
+                char e = (char)(c+2);
+                writer.write(e);
+            }
+            reader.close();
+            writer.close();
+            System.out.println("Encrypted message is written to encrypted.txt");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+    }
+}
---
+++ dayFive/Prime.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[]args) {
+        String inputFile = "numbers.txt";
+        String outputFile = "prime.txt";
+        try{
+            FileReader reader = new FileReader(inputFile);
+            FileWriter writer = new FileWriter(outputFile);
+            Scanner scanner = new Scanner(reader);
+            while(scanner.hasNextInt()) {
+                int num=scanner.nextInt();
+                int i,flag=0;
+                for(i=2;i<=(num/2);i++) {
+                    if(num%i==0) {
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==0) {
+                    writer.write(num+"\n");
+                }
+            }
+            reader.close();
+            writer.close();
+
+            System.out.println("Prime numbers are written to prime.txt");
+        }
+        catch(IOException e) {
+                System.out.println("An error occurred.");
+            }
+    }
+}
